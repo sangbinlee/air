de62bbf66caa0a9d88f43991c2555c831afed8bc
sangbinlee9 <sangbinlee9@smartscore.kr>
2024-04-05 17:54:18 +0900

    cors 에러 남

--- src/main/java/com/topas/air/AirApplication.java
+++ src/main/java/com/topas/air/AirApplication.java
@@ -2,6 +2,9 @@ package com.topas.air;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AirApplication {
@@ -10,4 +13,16 @@ public class AirApplication {
 		SpringApplication.run(AirApplication.class, args);
 	}
 
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+				registry.addMapping("*").allowedOrigins("*");
+			}
+		};
+	}
 }
