080be85e1e581f0bfb500154bff8c7083d6fd72a
sangbinlee <sangbinlee9@gmail.com>
2024-04-11 21:01:48 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,13 @@
-DROP TABLE t_USERS;
+-- https://github.com/spring-projects/spring-batch/blob/4.3.x/spring-batch-core/src/main/resources/org/springframework/batch/core/schema-oracle10g.sql
+DROP TABLE  t_USERS;
+
+
+ 
 create table t_USERS(
   ID int not null ,
   NAME varchar(100) not null,
   STATUS int,
   PRIMARY KEY ( ID )
 );
+
+ 
